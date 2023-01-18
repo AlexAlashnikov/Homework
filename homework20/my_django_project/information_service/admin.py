dd30ea9981bdfa1c39cc7e05ea0750a741631bf3
AlexAlashnikov <116827750+AlexAlashnikov@users.noreply.github.com>
2023-01-18 22:52:32 +0300

    Delete homework20/my_django_project directory

--- homework20/my_django_project/information_service/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from information_service.models import *
-
-admin.site.register(User)
-admin.site.register(Category)
-admin.site.register(Post)
--- homework20/my_django_project/information_service/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class InformationServiceConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'information_service'
